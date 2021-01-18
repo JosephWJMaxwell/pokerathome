d6e752d84ec0e3ad187fd8323236410c0bc41c6e
Jmaxwell <jwjmaxwell@outlook.com>
2021-01-17 20:54:37 -0500

    fixing and adding export cmd

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,73 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+/* pages/index.js */
+    
+import React, { Component } from 'react';
+import Layout from '../components/Layout';
+import Chat from '../components/Chat';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+class IndexPage extends Component {
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+  state = { user: null }
+  
+  handleKeyUp = evt => {
+    if (evt.keyCode === 13) {
+      const user =  evt.target.value;
+      this.setState({ user });
+    }
+  }
+  
+  render() {
+    const { user } = this.state;
+    
+    const nameInputStyles = {
+      background: 'transparent',
+      color: '#999',
+      border: 0,
+      borderBottom: '1px solid #666',
+      borderRadius: 0,
+      fontSize: '3rem',
+      fontWeight: 500,
+      boxShadow: 'none !important'
+    };
+    
+    return (
+      <Layout pageTitle="Realtime Chat">
+      
+        <main className="container-fluid position-absolute h-100 bg-dark">
+        
+          <div className="row position-absolute w-100 h-100">
+          
+            <section className="col-md-8 d-flex flex-row flex-wrap align-items-center align-content-center px-5">
+              <div className="px-5 mx-5">
+              
+                <span className="d-block w-100 h1 text-light" style={{marginTop: -50}}>
+                  {
+                    user
+                      ? (<span>
+                          <span style={{color: '#999'}}>Hello!</span> {user}
+                        </span>)
+                      : `What is your name?`
+                  }
+                </span>
+                
+                { !user && <input type="text" className="form-control mt-3 px-3 py-2" onKeyUp={this.handleKeyUp} autoComplete="off" style={nameInputStyles} /> }
+                
+              </div>
+            </section>
+            
+            <section className="col-md-4 position-relative d-flex flex-wrap h-100 align-items-start align-content-between bg-white px-0">
+              { user && <Chat activeUser={user} /> }
+            </section>
+            
+          </div>
+          
+        </main>
+        
+      </Layout>
+    );
+  }
+  
 }
+
+export default () => (
+  <IndexPage />
+);
